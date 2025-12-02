524aa5038b9e79b4ee5dea963c0e43d71f91e05b
mpJunot <junot-hery-nantenaina.montpre@epitech.eu>
2025-12-02 19:48:10 +0100

    feat(ci-backend): fix lint errorclear

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +15,30 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/graphql (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should serve GraphQL playground', () => {
     return request(app.getHttpServer())
       .get('/graphql')
       .expect(200);
+  });
+
+  it('should handle basic GraphQL query', () => {
+    const query = `
+      query {
+        __typename
+      }
+    `;
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.__typename).toBe('Query');
+      });
   });
 });
 
