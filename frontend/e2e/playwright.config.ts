f3c1a57fdb809de7bc5619d4e07d6290b651af69
Benjamin Maillot <benjamin.maillot@epitech.eu>
2026-01-20 20:13:00 +0100

    creat test e2e for forgot password, reset password, register

--- frontend/e2e/playwright.config.ts
+++ frontend/e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 const baseURL = process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000';
 
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: './tests',
   timeout: 60_000,
   expect: {
     timeout: 10_000,
